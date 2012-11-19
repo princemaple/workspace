b8c748196b705fc6c41a44c54bf746d83d90b759
princemaple <chenpaul914@gmail.com>
2012-11-19 18:40:13 +1100

    update LogicalSymbol.py remove snake html5 game from this repo

--- LogicalSymbol.py
+++ LogicalSymbol.py
@@ -1,19 +1,25 @@
+# coding=utf-8
+
+import re
+
 class LogicalSymbol(object):
 	"""\
 	To replace special marks by corresponding logical symbols
 
-	@imply -> u'\u21d2'
-	@unify -> u'\u2192'
-	@biimp -> u'\u21d4'
-	@equal -> u'\u2261'
-	@conj  -> u'\u2227'
-	@disj  -> u'\u2228'
-	@xor   -> u'\u2295'
-	@tauto -> u'\u22a4'
-	@contr -> u'\u22a5'
-	@univs -> u'\u2200'
-	@exist -> u'\u2203'
-	@consq -> u'\u22a8'\
+	@imply -> u'\u21d2' ⇒
+	@unify -> u'\u2192' →
+	@biimp -> u'\u21d4' ⇔
+	@equal -> u'\u2261' ≡
+	@not   -> u'\u00ac' ¬
+	@conj  -> u'\u2227' ∧
+	@disj  -> u'\u2228' ∨
+	@xor   -> u'\u2295' ⊕
+	@tauto -> u'\u22a4' ⊤
+	@contr -> u'\u22a5' ⊥
+	@univs -> u'\u2200' ∀
+	@exist -> u'\u2203' ∃
+	@consq -> u'\u22a8' ⊨
+	@empty -> u'\u2205' Ø
 	"""
 	def __init__(self, context=None):
 		super(LogicalSymbol, self).__init__()
@@ -27,11 +33,12 @@ class LogicalSymbol(object):
 			"@conj"  : u' \u2227 ',
 			"@disj"  : u' \u2228 ',
 			"@xor"   : u' \u2295 ',
-			"@tauto" : u'\u22a4',
-			"@contr" : u'\u22a5',
+			"@tauto" : u' \u22a4 ',
+			"@contr" : u' \u22a5 ',
 			"@univs" : u'\u2200',
 			"@exist" : u'\u2203',
-			"@consq" : u' \u22a8 '
+			"@consq" : u' \u22a8 ',
+			"@empty" : u' \u2205 '
 		}
 		self.result = None
 
@@ -45,16 +52,44 @@ class LogicalSymbol(object):
 		f.close()
 		self.result = None
 
+	def de_utf8(self):
+		"""
+		If input string/file is in UTF-8 coding,
+		it has to be decoded into ascii.
+		"""
+		self.content = self.content.decode("utf8")
+
 	def process(self, context = None):
 		if context: self.read(context)
 		if not self.content: return None
-		import re
-		processed = self.content
+		processed = self.fromRaw(self.content)
 		for symbol in self.__data:
-			processed = re.sub(" *"+symbol+" *",
-				self.__data[symbol], processed)
+			try:
+				processed = re.sub(" *"+symbol+" *",
+					self.__data[symbol], processed)
+			except:
+				print("Expecting ascii input")
+				raw_input()
+				return None
 		self.result = processed
 		return processed
+
+	def fromRaw(self, content):
+		data = [
+			(r"<=>",  "@biimp"),
+			(r"=>" ,  "@imply"),
+			(r"->" ,  "@unify"),
+			(r"<\+>", "@xor"),
+			(r"~"  ,  "@not"),
+			(r"\ball\b",  "@univs"),
+			(r"\bany\b",  "@exist"),
+			# ("\band\b",  "@conj"),
+			# ("\bor\b",   "@disj"),
+			(r"\{\}", "@empty")
+		]
+		for pair in data:
+			content = re.sub(pair[0], pair[1], content)
+		return content
 
 	def htmlize(self):
 		template = """<!doctype html>\n<html lang="en">\n<head>\n\t<meta charset="UTF-8">\n\t<title>LogicalSymbol Process Result</title>\n</head>\n<body>\n\t<div style="width:800px; margin: auto;">{{content}}</div>\n</body>\n</html>"""
