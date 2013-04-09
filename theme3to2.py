5071bc3610d18e82787e17c8e0682e55641c3874
princemaple <chenpaul914@gmail.com>
2013-04-09 12:25:24 +1000

    updated theme3to2.py, for easier use

--- theme3to2.py
+++ theme3to2.py
@@ -1,10 +1,9 @@
 import re
 
 with open("XDefault.sublime-theme", "r") as f:
-	w = open("XDefault2.sublime-theme", "w")
 	t = f.read().replace("\n", "@")
 	s = "// transient property not needed"
 	t = re.sub(r"\{[^{]+?transient.+?\},", s, t)
 	t = t.replace("@", "\n")
-	w.write(t)
-	w.close()
+	f.write(t)
+	f.close()
